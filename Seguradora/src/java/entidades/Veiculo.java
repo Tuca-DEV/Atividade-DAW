f124e102a7de207c7a42ce510ad57b743b793648
Tuca <guga_camil@hotmail.com>
2021-08-13 10:08:32 -0300

    Projeto final - Seguradora
    Mesma estrutura do ClubeCinemaDB, porém com tema diferente. No caso, agora se trata de veículos de uma seguradora.

---
+++ Seguradora/src/java/entidades/Veiculo.java
@@ -0,0 +1,74 @@
+
+/**
+ *
+ * @author guga_
+ */
+package entidades;
+
+public class Veiculo {
+    
+    private String dono;
+    private String placa;
+    private String marca;
+    private String ano;
+    private String modelo;
+
+    public Veiculo(String dono, String marca, String placa, String ano, String modelo) {
+        this.dono = dono;
+        this.marca = marca;
+        this.placa = placa;
+        this.ano = ano;
+        this.modelo = modelo;
+    }
+    
+    public Veiculo(){
+        
+    }
+    
+    @Override
+    public String toString(){
+        return  ano + " - " + placa + " - " + modelo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+      
+}
